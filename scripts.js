c0d3a92b491cb652be8167851d26f1a3614906bd
Jake Conwell <jakelconwell@gmail.com>
2023-02-21 15:27:15 -0600

    finished coding project

--- scripts.js
+++ scripts.js
@@ -10,13 +10,17 @@ function controlRocket() {
     const shuttleBackground = document.getElementById("shuttleBackground");
     const miles = document.getElementById("spaceShuttleHeight");
     const rocketShip = document.getElementById("rocket");
+    const rocketHeight = miles.innerHTML;
+    let currentHeight = Number(rocketHeight);
 
     takeoffButton.addEventListener("click", () => {
         let confirm = window.confirm("Confirm that the shuttle is ready for takeoff");
         if (confirm) {
             flightStatus.innerHTML = "Shuttle in flight";
             shuttleBackground.style.background = "blue";
-            miles.innerHTML = "10,000";
+            miles.innerHTML = "10000";
+            rocketShip.style.top = "250px";
+            rocketShip.style.left = "350px";
         };
     });
 
@@ -25,6 +29,8 @@ function controlRocket() {
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.background = "green";
         miles.innerHTML = "0";
+        rocketShip.style.top = "250px";
+        rocketShip.style.left = "350px";
     });
 
     abortButton.addEventListener("click", ()=> {
@@ -33,6 +39,41 @@ function controlRocket() {
             flightStatus.innertHTML = "Mission aborted.";
             shuttleBackground.style.background = "green";
             miles.innerHTML = "0";
+            rocketShip.style.top = "250px";
+            rocketShip.style.left = "350px";
+        };
+    });
+
+    rocketShip.style.top = "250px";
+    rocketShip.style.left = "350px";
+
+    downButton.addEventListener("click", ()=> {
+        if (parseInt(rocketShip.style.top) < 250) {
+            rocketShip.style.top = parseInt(rocketShip.style.top) + 10 + 'px';
+            miles.innerHTML = currentHeight -= 10000;
+            // console.log(rocketShip.style.top);
+        };
+    });
+
+    upButton.addEventListener("click", ()=> {
+        if (parseInt(rocketShip.style.top) > -10) {
+            rocketShip.style.top = parseInt(rocketShip.style.top) - 10 + 'px';
+            miles.innerHTML = currentHeight += 10000;
+            // console.log(rocketShip.style.top);
+        };
+    });
+
+    leftButton.addEventListener("click", ()=> {
+        if (parseInt(rocketShip.style.left) > -20) {
+            rocketShip.style.left = parseInt(rocketShip.style.left) - 10 + 'px';
+            console.log(rocketShip.style.left);
+        };
+    });
+
+    rightButton.addEventListener("click", ()=> {
+        if (parseInt(rocketShip.style.left) < 710) {
+            rocketShip.style.left = parseInt(rocketShip.style.left) + 10 + 'px';
+            console.log(rocketShip.style.left);
         };
     });
 
